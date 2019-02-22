906dcf38ff2d4a84af6805871b99c041e5d976c1
Wizard <lsw1991abc@gmail.com>
2019-02-22 14:43:07 +0800

    增加序列化工具及打包时包含源码

---
+++ src/main/java/cloud/mobe/boot/utils/JsonUtil.java
@@ -0,0 +1,84 @@
+package cloud.mobe.boot.utils;
+
+import static cloud.mobe.boot.utils.CheckEmptyUtil.isEmpty;
+import static cloud.mobe.boot.utils.CheckEmptyUtil.isOrEmpty;
+
+import cloud.mobe.boot.utils.json.MobeObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Json工具类.
+ *
+ * <p>用于json和pojo之间的换换
+ *
+ * @author lsw1991abc@gmail.com
+ * @since 2019-02-21 10:33
+ */
+@Slf4j
+public final class JsonUtil {
+
+  /**
+   * 将一个对象转换为json字符串.
+   *
+   * @param value 要转换的对象
+   * @return 转换失败返回null, 否则返回json字符串
+   */
+  public static String getJsonString(Object value) {
+    if (isEmpty(value)) {
+      return null;
+    }
+    MobeObjectMapper mapper = new MobeObjectMapper();
+    try {
+      return mapper.writeValueAsString(value);
+    } catch (JsonProcessingException ex) {
+      log.error("error when getJsonString! " + ex.getMessage());
+      return null;
+    }
+  }
+
+  /**
+   * 将一个json字符串转换为对象.
+   *
+   * @param json 要转换的json字符串
+   * @param clz 转换的对象类型
+   * @return 转换失败返回null, 否则返回对象
+   * @see #parseJsonString(String, TypeReference)
+   */
+  public static <T> T parseJsonString(String json, final Class<T> clz) {
+    return parseJsonString(
+        json,
+        isEmpty(clz)
+            ? null
+            : new TypeReference<T>() {
+              @Override
+              public Type getType() {
+                return clz;
+              }
+            });
+  }
+
+  /**
+   * 将一个json字符串转换为对象.
+   *
+   * @param json 要转换的json字符串
+   * @param ref 转换的对象类型 {@code new TypeReference<String>() {}}
+   * @return 转换失败返回null, 否则返回对象
+   * @see #parseJsonString(String, Class)
+   */
+  public static <T> T parseJsonString(String json, TypeReference<T> ref) {
+    if (isOrEmpty(json, ref)) {
+      return null;
+    }
+    MobeObjectMapper mapper = new MobeObjectMapper();
+    try {
+      return mapper.readValue(json, ref);
+    } catch (IOException ex) {
+      log.error("error when parseJsonString! " + ex.getMessage());
+      return null;
+    }
+  }
+}
